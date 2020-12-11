b88674fbe222b94e97c897a103c9445fb73aa692
Egor <egorin99@bk.ru>
2020-12-11 14:00:35 +0300

    [upd] первая домашка

---
+++ lesson_1/lesson_1_1.py
@@ -0,0 +1,16 @@
+one = "1"
+print(one)
+
+helo = 'Привет'
+print(helo)
+
+one = int(one)
+
+print(type(one))
+
+word = input('Введите слово: ')
+print(word)
+
+number = input('Введите номер: ')
+print(number)
+
